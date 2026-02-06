fc2e0ad465a1c82ac5b8e253da52aa49c615290e
vitorpicolotto <picolottovitor@gmail.com>
2026-02-06 13:27:23 -0300

    Exercicio 1

---
+++ Condicionais/lista-condicionais.js
@@ -0,0 +1,5 @@
+/*
+    Exercício 1
+    Crie um programa que, dado o valor da idade, verifique se a pessoa pode ou não entrar no evento. Exiba a mensagem informando se o acesso foi ou não permitido.
+*/
+
