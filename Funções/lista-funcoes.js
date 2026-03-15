b6dda3e0f8e5f20facf417ede4082fa57ca29d2f
vitorpicolotto <picolottovitor@gmail.com>
2026-03-15 10:57:24 -0300

    continuação da lista de funções

--- Funções/lista-funcoes.js
+++ Funções/lista-funcoes.js
@@ -19,3 +19,30 @@ function calculaDesconto(valorOriginal, desconto){
 }
 
 console.log(calculaDesconto(100, 10))
+
+// 3 - Calcular a média aritmética de um aluno utilizando uma arrow function
+
+let calculaMedia = (nota1, nota2) => {
+    let mediaAritmetica = (nota1 + nota2) / 2;
+    return mediaAritmetica;
+}
+
+
+console.log('Sua nota média é = ', calculaMedia(10,8))
+
+// 4 - Sistema de quiz que mostra se o número digitado é par ou é impar - com a lógica encapsulada em uma arrow function
+
+let verificarParidade = (valorNumero) => {
+    if (valorNumero % 2 === 0){
+        console.log('Número par')
+    } else {
+        console.log('Número impar')
+    }
+}
+
+verificarParidade(6);
+verificarParidade(7);
+
+
+
+
