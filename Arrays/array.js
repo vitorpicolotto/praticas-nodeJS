0257a1a7628e53e8d06ed5b7b579bf0ae0f44e59
vitorpicolotto <picolottovitor@gmail.com>
2026-03-29 18:36:12 -0300

    exemplos de arrays

---
+++ Arrays/array.js
@@ -0,0 +1,17 @@
+const array = [1, 2, 3, 4];
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
+array[0] = 3;
+console.log(array)
+
+array[5] = 5; //array esparso
+console.log(array);
+
+
+
+
+
+
