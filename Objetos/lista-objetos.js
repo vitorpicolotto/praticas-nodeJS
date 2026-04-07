c41ef7783cc1d16e395f20e737a8eacbf6fcbbcb
Vitor Picolotto <picolottovitor@gmail.com>
2026-04-07 17:00:09 -0300

    Concluindo os desafios da lista

--- Objetos/lista-objetos.js
+++ Objetos/lista-objetos.js
@@ -111,4 +111,115 @@ let dadosParaObjeto = [
 
 console.log(montarObjeto(dadosParaObjeto));
 
-// 8 - 
+// 8 - Criar um sistema de inspeção, onde cada máquina inspecionada gera automaticamente um relatório de medições com dados de TEMPERATURA, VIBRAÇÃO, PRESSÃO e NÍVEL DE RUÍDO. Informações são organizadas dentro de um objeto, onde cada chave representa uma categoria avaliada e o valor representa o nível na inspeção.
+// Exibir as categorias, os valores, percorrer as medições (chave/valor) e imprimir detalhadamente com STATUS (> 50 = alerta; < 50 = ok.)
+
+let medicoes = {
+    temperatura: 47,
+    vibracao: 29,
+    pressao: 33,
+    ruido: 94
+};
+
+let categorias = Object.keys(medicoes);
+let valores = Object.values(medicoes);
+let entradas = Object.entries(medicoes);
+
+console.log('Categorias avaliadas:', categorias);
+console.log('Valores:', valores);
+console.log('\nDetalhes:');
+
+for(let [chave, valor] of entradas){
+    let status = valor > 50 ? 'Alerta!!' : 'Ok';
+    console.log(`${chave}: ${valor} (${status})`);
+}
+
+
+// 9 - Relatório de manutenção preventiva - cada máquina registra os meses que passou por manutenção e a quantidade de dias parados em um objeto, onde cada chave representa um mês e o valor representa os dias de inatividade. Criar sistema para gerar um relatório que exiba: total de dias parados, quantidade de meses com manutenção, alerta caso o total de dias parados seja maior que 20 dias
+
+let diasDeManutencao = {
+    janeiro: 2,
+    fevereiro: 0,
+    marco: 9,
+    abril: 1,
+    maio: 0,
+    junho: 0,
+    julho: 2,
+    agosto: 6,
+    setembro: 0,
+    outubro: 4,
+    novembro: 1,
+    dezembro: 0
+}
+
+//for...in porque itera diretamente sobre todas as propriedades enumeráveis de um objeto
+
+let totalDeDias = 0;
+let mesesComParada = 0;
+
+for (let mes in diasDeManutencao){
+    let dias = diasDeManutencao[mes];
+    totalDeDias += dias;
+
+    if(dias > 0){
+        mesesComParada++;
+    }
+}
+
+console.log(`Total de dias parados: ${totalDeDias}`);
+console.log(`Meses com manutenção registrada: ${mesesComParada}`);
+
+if(totalDeDias > 20){
+    console.log('Status: Atenção! Acima do limite anual!');
+} else {
+    console.log('Status: Dentro do limite anual');
+}
+
+
+// 10 - Analisando desempenho dos estudantes, onde cada curso possui uma lista de estudantes com seus respectivos dados de progresso. Construir um relatório que mostra desempenho geral da turma. 
+/*
+    Crie um objeto chamado curso, com as seguintes propriedades:
+        titulo: string com o nome do curso;
+        estudantes: um array de objetos, cada um com nome e progresso (número de 0 a 100);
+        Um método interno chamado gerarRelatorio.
+
+    O método gerarRelatorio deve:
+        Percorrer o array de estudantes;
+        Exibir no console o nome de cada estudante e sua situação:
+            Se o progresso for igual ou maior que 70, mostrar "Aprovado";
+            Senão, mostrar "Em andamento";
+        Ao final, mostrar:
+            A quantidade total de estudantes;
+            A média geral de progresso da turma.
+*/
+
+let curso = {
+  titulo: "JavaScript Intermediário",
+  estudantes: [
+    { nome: "Ana", progresso: 85 },
+    { nome: "Carlos", progresso: 40 },
+    { nome: "Juliana", progresso: 72 },
+    { nome: "Pedro", progresso: 60 }
+  ],
+  gerarRelatorio: function (){
+    let totalProgresso = 0;
+
+    for(let estudante of this.estudantes){
+        let situacao = estudante.progresso >= 70 ? 'Aprovado' : 'Em andamento';
+        console.log(`Estudante: ${estudante.nome} | Progresso: ${estudante.progresso}% | Situação: ${situacao}`);
+        totalProgresso += estudante.progresso
+    }
+
+    let mediaDaTurma = (totalProgresso / this.estudantes.length);
+    console.log(`\nTotal de estudantes: ${this.estudantes.length}`);
+    console.log(`Média geral da turma: ${mediaDaTurma}%`);
+  }
+};
+
+curso.gerarRelatorio();
+
+
+
+
+
+
